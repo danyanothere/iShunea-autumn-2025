01fcf6c1f30c31691c19c1ff356cc290b0a345d4
danyanothere <2gdanyd@gmail.com>
2025-09-03 14:43:56 +0300

    Update Footer.tsx

--- Footer.tsx
+++ Footer.tsx
@@ -5,43 +5,55 @@ import { footerLinks } from "@/constants";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-        <div className="flex flex-col justify-start items-start gap-6">
-          <Image 
-            src="/logo.svg" 
-            alt="logo"
-            width={118} 
-            height={18} 
-            className="object-contain"
-          />
-          <p className="text-base text-gray-700">
-            Carhub 2025 <br />
-            All rights reserved &copy; 
-          </p>
-        </div>
-        
-        <div className="footer__links flex-1 w-full flex md:justify-end flex-wrap max-md:mt-10 gap-20"> 
-          {footerLinks.map((link) => (
-            <div key={link.title} className="footer__link flex flex-col gap-6 text-base min-w-[170px]">
-              <h3 className="font-bold">{link.title}</h3>
-              <div className="flex flex-col gap-3">
+    <footer className="flex flex-col text-black-100
+    mt-5 border-t border-gray-100">
+        <div className="flex max-md:flex-col
+        flex-wrap justify-between gap-5
+        sm:px-16 px-6 py-10">
+            <div className="flex flex-col justify-start
+            items-start gap-6">
+                <Image src= "/logo.svg" alt = "logo" width={118} height={18}
+                className="object-contain"/>
+                <p className="text-base text-gray-700">
+                    Carhub 2025 <br/>
+                    All rights reserved &copy;
+                </p>
+            </div>
+            <div className="footer__links">
+            {footerLinks.map((link) => (
+            <div key ={link.title} className="footer__link">
+                <h3 className="font-bold">{link.title}</h3>
                 {link.links.map((item) => (
-                  <Link 
+                    <Link
                     key={item.title}
                     href={item.url}
-                    className="text-gray-500 hover:text-gray-700"
-                  >
+                    className="text-gray-500" >
                     {item.title}
-                  </Link>
+                    </Link>
                 ))}
-              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </footer>
-  );
+            ))}
+            </div>
+            </div>
+            <div className="flex justify-between
+            items-center flex-wrap mt-10 border-t
+            border-gray-100 sm:px-16 px-6 py-10">
+                <p>@2025 CarHub. All Rights Reserved</p>
+
+                <div className="footer__copyrights-link">
+                <Link href = "/" className="text-gray-500">
+                    Privacy Policy
+                </Link>
+                <Link href = "/" className="text-gray-500">
+                    Terms of Use
+                </Link>
+
+                </div>
+            </div>
+    
+        
+        </footer>
+  )
 }
 
-export default Footer;
+export default Footer

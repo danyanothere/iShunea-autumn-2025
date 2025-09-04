46f1f24d5375b87c2b35c60bb513ff64ede7524a
danyanothere <2gdanyd@gmail.com>
2025-09-04 16:13:47 +0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-import Hero from "./Hero";
-import CustomButton from "./CustomButton";
-import { Navbar } from "./Navbar";
-import Footer from "./Footer";
+export async function fetchCars() {
+     const headers= {
+        'x-rapidapi-key': 'd047ba1459mshdb1af88804330afp17fc43jsnfcfb45fe2fa7',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+     }
 
+     const response = await fetch  ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+        { headers: headers,
+});
 
-export { 
-    Hero, CustomButton,
-    Navbar, Footer
+const result = await response.json();
+
+return result;
 
 };

197452519f55590ce882bd7474078f6a1ca046f3
danyanothere <2gdanyd@gmail.com>
2025-09-05 03:42:20 +0300

    Update CustomButton.tsx

--- CustomButton.tsx
+++ CustomButton.tsx
@@ -1,20 +1,30 @@
 "use client";
 import { CustomButtonProps } from '@/types';
-import image from 'next/image';
+import Image from 'next/image';
 
 const CustomButton = ({title, containerStyles, 
-    handleClick, btnType}: CustomButtonProps ) => {
+    handleClick, btnType, textStyles, rightIcon }: CustomButtonProps ) => {
   return (
     <button 
     disabled={false}
     type={btnType || "button"}
-    className={'custom-btn ${containerStyles}'}
+    className={`custom-btn ${containerStyles} flex items-center justify-center gap-2`} 
     onClick={handleClick}
     >
-        <span className={'flex-1'}>
+        <span className={`flex-1 ${textStyles}`}> 
             {title}
         </span>
-        </button>
+        {rightIcon && (
+          <div className="relative w-6 h-6">
+            <Image 
+              src={rightIcon}
+              alt="right icon"
+              fill 
+              className="object-contain"
+            />
+          </div>
+        )}
+    </button>
   )
 }
 

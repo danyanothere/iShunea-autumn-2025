eade2f3e36de0e7f16d915684d41d037ac19f2c2
danyanothere <2gdanyd@gmail.com>
2025-09-05 03:44:33 +0300

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
      const headers= {
         'x-rapidapi-key': 'd047ba1459mshdb1af88804330afp17fc43jsnfcfb45fe2fa7',
 		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
      }
 
-     const response = await fetch  ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
+     const response = await fetch  ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
         { headers: headers,
 });
 
@@ -13,3 +15,18 @@ const result = await response.json();
 return result;
 
 };
+export const calculateCarRent = (city_mpg: number, year: number): string => {
+const basePricePerDay = 50; // Base rental price per day in dollars
+const mileageFactor = 0.1; // Additional rate per mile driven
+const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+const mileageRate = city_mpg * mileageFactor;
+const ageRate = (new Date().getFullYear() - year) * ageFactor;
+const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = (car: CarProps, angle? : string) => {
+
+
+}

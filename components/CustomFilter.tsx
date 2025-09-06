959ebb9fcf2c0dc6a23c4f17a20f93ab37c15648
danyanothere <2gdanyd@gmail.com>
2025-09-06 13:20:27 +0300

    Update CustomFilter.tsx
    added search filters

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
 
-const CustomFilter = () => {
-  return (
-    <div>CustomFilter</div>
-  )
+import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { fuels, yearsOfProduction } from "@/constants";
+
+interface CustomFilterProps {
+  title: "fuel" | "year";
 }
 
-export default CustomFilter
+const CustomFilter: React.FC<CustomFilterProps> = ({ title }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const current = searchParams.get(title) || "";
+
+  const options = title === "fuel" ? fuels : yearsOfProduction;
+
+  const onChange = (value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(title, value);
+    } else {
+      params.delete(title);
+    }
+    router.push(`/?${params.toString()}`);
+  };
+
+  return (
+    <div className="min-w-[160px]">
+      <label className="block text-sm font-medium text-gray-700 mb-1 capitalize">
+        {title}
+      </label>
+      <select
+        className="filter-select"
+        value={current}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {options.map((opt) => (
+          <option key={opt.title} value={opt.value}>
+            {opt.title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CustomFilter;
